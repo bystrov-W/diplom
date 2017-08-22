4e9a79b3adba45c6ec88cc40839f9280a0ba14a4
Быстров Владимир Владимирович <VBystrov@verdom.local>
2017-08-22 10:00:33 +0300

    create project

---
+++ templates/footer.php
@@ -0,0 +1,9 @@
+    <footer class="footer">
+      <div class="container">
+		<hr>
+		<br>
+        <p class="text-muted">Юрий Михин, 2017. Netology.ru, PHP-6</p>
+      </div>
+    </footer>
+	</body>
+</html>
