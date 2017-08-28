3cccd9ecc2ab7d25495a051f2ffbc15daaadf9c1
bystrov-W <bystrov.v.v@outlook.com>
2017-08-28 19:21:01 +0300

    Add files via upload

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -1,15 +1,19 @@
-<?php
-
+﻿<?php
+// файл Controller.php
 class Controller
 {
     protected $pdo;
     protected $view;
-
+ 
     public function __construct()
     {
-        $this->pdo = new Database();
+        $dsn = 'mysql:dbname=diplom;host=localhost';
+        $user = 'root';
+        $pass = '';
+ 
+        $this->pdo = new PDO($dsn, $user, $pass);
         $this->view = new Template();
-
+ 
         //Редиректы
         //если не авторизован
         if (empty($_SESSION['user']) && $_SERVER['REQUEST_URI'] == '/question/index' OR empty($_SESSION['user']) && $_SERVER['REQUEST_URI'] == '/question-edit.php') {
@@ -21,3 +25,6 @@ class Controller
         }
     }
 }
+ 
+ 
+?>
