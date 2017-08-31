f7af818182e847b174dde575f53a8742f5e29396
bystrov-W <bystrov.v.v@outlook.com>
2017-09-01 00:37:20 +0300

    Update Model.php

--- models/Model.php
+++ models/Model.php
@@ -1,11 +1,13 @@
-<?php
-
 class Model
 {
     protected $pdo;
-
+ 
     public function __construct()
     {
-        $this->pdo = new Database();
+        $dsn = 'mysql:dbname=test;host=127.0.0.1';
+        $user = 'root';
+        $pass = '';
+ 
+        $this->pdo = new PDO($dsn, $user, $pass);
     }
 }
